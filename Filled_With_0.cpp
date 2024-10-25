17bbc06b4b799d141cf707fef5e2ba07f2919ebb
RikuUnno <riku.unno@gmail.com>
2024-10-25 11:33:43 +0900

    プロジェクト ファイルを追加します。

---
+++ Filled_With_0.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void Show(int* time, int& seconds, int& minutes, const int ONE_MINUTE );
+void FilledWith0(int* time, int& seconds, int& minutes, const int ONE_MINUTE );
+
+void FilledWith0(int *time, int& seconds, int& minutes, const int ONE_MINUTE)
+{
+	if (*time >= 60)
+	{
+		minutes = *time / ONE_MINUTE;
+		seconds = *time % ONE_MINUTE;
+	}
+	Show(time, seconds, minutes, ONE_MINUTE);
+}
+
+void Show(int *time, int& seconds, int& minutes, const int ONE_MINUTE)
+{
+	if (*time >= ONE_MINUTE)
+	{
+		cout << minutes << ':' << flush;
+		if (0 <= seconds && seconds < 10 || *time % ONE_MINUTE == 0)
+		{
+			cout << setw(2) << setfill('0') << flush;
+		}
+		cout << seconds << "秒" << endl;
+	}
+	else
+	{
+		cout << "0:" << flush;
+		if (0 <= *time && *time < 10)
+		{
+			cout << setw(2) << setfill('0') << flush;
+		}
+		cout << *time << "秒" << endl;
+	}
+}
+
+int main()
+{
+	int time = 0;
+	int seconds = 0;
+	int	minutes = 0;
+	const int ONE_MINUTE = 60;
+
+	cout << "秒数を入力してください。" << endl;
+	cin >> time;
+
+	cout << endl;
+
+	FilledWith0(&time, seconds, minutes, ONE_MINUTE);
+}
